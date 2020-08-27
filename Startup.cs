f164ed27ad36237f8024c0d194e1e7a89cec8e9e
mangeletti12 <mark.angeletti@gmail.com>
2020-08-27 09:26:42 -0600

    health check

--- Startup.cs
+++ Startup.cs
@@ -26,6 +26,12 @@ namespace mka_one
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            // mka pg 105
+            services.AddHealthChecks()
+                .AddCheck("ICMP_01", new ICMPHealthCheck("www.ryadel.com", 100))
+                .AddCheck("ICMP_02", new ICMPHealthCheck("www.google.com", 100))
+                .AddCheck("ICMP_03", new ICMPHealthCheck("www.does-not-exist.com", 100));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +56,9 @@ namespace mka_one
             }
 
             app.UseRouting();
+
+            // mka
+            app.UseHealthChecks("/hc", new CustomHealthCheckOptions());
 
             app.UseEndpoints(endpoints =>
             {
